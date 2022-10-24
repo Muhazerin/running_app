c13a26315299c1d05d0d00c41b3d17f4d6163d83
Muhazerin <muhd.muhazerin@gmail.com>
2022-10-24 18:11:20 +0800

    RAF-2: Added icons and welcome text to home page, Edited homepage components test

--- test/pages/homepage_test.dart
+++ test/pages/homepage_test.dart
@@ -4,12 +4,32 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:running_app/pages/homepage.dart';
 
 void main() {
-  testWidgets('Widgets existence test', (WidgetTester tester) async {
-    await tester.pumpWidget(const HomePage());
+  testWidgets('HomePage component existence test', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: HomePage(),
+      ),
+    );
 
     expect(
-        find.text('Welcome! What would you like to do today?'), findsOneWidget);
-    expect(find.byIcon(Icons.directions_run_rounded), findsOneWidget);
-    expect(find.byIcon(Icons.timer_rounded), findsOneWidget);
+      find.text('Welcome! What would you like to do today?'),
+      findsOneWidget,
+    );
+    expect(
+      find.byIcon(Icons.directions_run_rounded),
+      findsOneWidget,
+    );
+    expect(
+      find.text('Endurance Run'),
+      findsOneWidget,
+    );
+    expect(
+      find.byIcon(Icons.timer_rounded),
+      findsOneWidget,
+    );
+    expect(
+      find.text('Interval Run'),
+      findsOneWidget,
+    );
   });
 }
