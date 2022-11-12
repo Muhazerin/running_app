8a6bc1fe0d2d7bef0b45b036aa6f514e10d33076
Muhazerin <muhd.muhazerin@gmail.com>
2022-11-12 22:10:03 +0800

    RAF-3: Made Google Maps and extensible widget

--- lib/widgets/my_google_map_widget.dart
+++ lib/widgets/my_google_map_widget.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:running_app/model/my_position.dart';
 
 class MyGoogleMapWidget extends StatefulWidget {
   const MyGoogleMapWidget({super.key});
@@ -12,78 +13,44 @@ class MyGoogleMapWidget extends StatefulWidget {
 }
 
 class _MyGoogleMapWidgetState extends State<MyGoogleMapWidget> {
-  LatLng? currentLatLng;
   final Completer<GoogleMapController> _controller = Completer();
+  GoogleMapController? _mapController;
 
   @override
   void initState() {
     super.initState();
-    _getCurrentLoction();
+    MyPosition.startLocationStream(_locationStreamCallback);
+  }
+
+  @override
+  void dispose() {
+    MyPosition.disposeStream();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: currentLatLng == null
-            ? const Center(
-                child: CircularProgressIndicator(),
-              )
-            : GoogleMap(
-                mapType: MapType.normal,
-                initialCameraPosition: CameraPosition(
-                  target: currentLatLng!,
-                  zoom: 17.5,
-                ),
-                onMapCreated: (GoogleMapController controller) {
-                  _controller.complete(controller);
-                },
-              ),
+    return GoogleMap(
+      mapType: MapType.normal,
+      initialCameraPosition: const CameraPosition(
+        target: LatLng(1.384699, 103.944722),
+        zoom: 17.5,
       ),
+      onMapCreated: (GoogleMapController controller) {
+        _controller.complete(controller);
+        _mapController = controller;
+      },
+      myLocationEnabled: true,
+      myLocationButtonEnabled: false,
     );
   }
 
-  void _getCurrentLoction() async {
-    Position position = await _determinePosition();
-    setState(() {
-      currentLatLng = LatLng(position.latitude, position.longitude);
-    });
-  }
-
-  Future<Position> _determinePosition() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    // Test if location services are enabled.
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
-      return Future.error('Location services are disabled.');
-    }
-
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
-        return Future.error('Location permissions are denied');
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      // Permissions are denied forever, handle appropriately.
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
-    }
-
-    // When we reach here, permissions are granted and we can
-    // continue accessing the position of the device.
-    return await Geolocator.getCurrentPosition();
+  void _locationStreamCallback(Position position) async {
+    LatLng newLatLng = LatLng(position.latitude, position.longitude);
+    _mapController?.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(target: newLatLng, zoom: 17.5),
+      ),
+    );
   }
 }
